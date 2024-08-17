a0b10c6dd80e82114c02c17e1de0dc94c6d1738c
Juan Torres <torrescjuan@javeriana.edu.co>
2024-08-16 22:34:37 -0500

    first view activity done, need to adjust alignment

--- app/src/main/java/com/example/wanderlist/MainActivity.kt
+++ app/src/main/java/com/example/wanderlist/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.wanderlist
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val spinner:Spinner = findViewById(R.id.spinner_destinations)
+        spinner.onItemSelectedListener = this
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val level = parent?.selectedItem
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
